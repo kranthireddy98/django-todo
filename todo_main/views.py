b7c0837d3d38133195d6a74bce9aa2092ee49dad
Kranthi <kranthireddy.mekala@gmail.com>
2024-01-08 23:54:35 +0530

    django-todo app

--- todo_main/views.py
+++ todo_main/views.py
@@ -5,7 +5,9 @@ from todo.models import Task
 
 def home(request):
     activeTasks = Task.objects.filter(is_completed=False).order_by('-id')
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-id')
     context = {
-        "tasks" :activeTasks
+        "tasks" :activeTasks,
+        "completedTasks" : completed_tasks
     }
     return render(request,'home.html',context)
